3949e6be6ee530f79e2ff80c30c7b797ee422a24
David Chan <72647287+Luckder@users.noreply.github.com>
2025-02-22 02:04:19 +0800

    Delete navbar.js

--- navbar.js
+++
@@ -1,5 +0,0 @@
-fetch('/_includes/navbar.html') // Or the correct path
-  .then(response => response.text())
-  .then(html => {
-    document.getElementById('navbar-container').innerHTML = html;
-  });
